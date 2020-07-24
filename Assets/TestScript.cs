67213acd5eb37f4af0130cf7975a171238b00e79
Noixelfer <pauldarius98@gmail.com>
2020-07-24 17:24:13 +0300

    Feat: Part 1

---
+++ Assets/TestScript.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+	// Start is called before the first frame update
+	void Start()
+	{
+	}
+
+	public int Add(int a, int b)
+	{
+		return a + b;
+	}
+
+	public int Multiply(int a, int b)
+	{
+		return a * b;
+	}
+}
